5bd0ec7a10c15822234b8a954385fca78bdde01f
庄骥南 <a492073467@gmail.com>
2020-08-29 18:51:23 +0800

    官网框架搭建

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Switch 示例</div>
+</template>
+
+<script>
+export default {
+name: "SwitchDemo"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import './index.scss'
-import {createWebHashHistory, createRouter} from 'vue-router'
-import Home from './components/Home.vue'
-import Doc from  './components/Doc.vue'
-
-const history = createWebHashHistory()
-const router = createRouter({
-    history,
-    routes: [
-        {path: '/', component: Home},
-        {path: '/doc', component: Doc}
-    ]
-})
+import {router} from './router'
 
 
 const app = createApp(App)
