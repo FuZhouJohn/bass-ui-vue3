91dcb672c75862f7f19f85a9985906f45796aaed
zhuangjinan <zhuangjinan2018@gmail.com>
2020-09-11 16:12:17 +0800

    button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>Button 示例</div>
+  <div>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick">你好</Button>
+  </div>
 </template>
 
-<script>
+<script lang='ts'>
+import Button from "../lib/Button.vue";
 export default {
-name: "ButtonDemo"
-}
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
