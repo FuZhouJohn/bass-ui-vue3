b0edda92b6d257fcaa92618e40711da546e4421f
zhuangjinan <zhuangjinan2018@gmail.com>
2020-09-30 15:37:44 +0800

    dialog 样式；接受 visible 属性；

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,14 +1,24 @@
 <template>
   <div>Dialog 示例</div>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">基础</Button>
+  <Dialog :visible="x"></Dialog>
 </template>
 
 <script lang="ts">
+import Button from "../lib/Button.vue";
 import Dialog from '../lib/Dialog.vue'
+import {ref} from 'vue'
 export default {
   name: "DialogDemo",
-  components: {Dialog}
+  components: {Dialog, Button},
+  setup() {
+    const x = ref(0)
+    const toggle = () => {
+      x.value = !x.value
+    }
+    return {x, toggle}
+  }
 }
 </script>
 
